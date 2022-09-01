90e954a6eae16208a50bb3492a07d5ff270bf51a
Andrew Boltachev <andrewboltachev@gmail.com>
2022-09-01 11:14:46 +0300

    allowed hosts

--- logicore/settings.py
+++ logicore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lvmdan_6bv(i*4x#$6vh$o3ink=er6s+pfk-&1!b6z#z1*m@2*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["andrewboltachev.club"]
 
 
 # Application definition
