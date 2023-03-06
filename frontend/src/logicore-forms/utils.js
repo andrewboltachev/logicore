35a1157879e4fc83270f0fd52d075891e3e8a127
Andrew Boltachev <andrewboltachev@gmail.com>
2023-03-07 02:26:12 +0300

    null

--- frontend/src/logicore-forms/utils.js
+++ frontend/src/logicore-forms/utils.js
@@ -8,65 +8,65 @@ extend("$autoArray", function (value, object) {
   return object ? update(object, value) : update([], value);
 });
 
-export const move = function(existing, element, delta) {
+export const move = function (existing, element, delta) {
   const array = [];
   const index = existing.indexOf(element);
   var newIndex = index + delta;
-  console.log('new index', index, delta, newIndex);
-  if ((newIndex > existing.length - 1) || (newIndex < 0)) {
-    console.log('move edge case');
+  console.log("new index", index, delta, newIndex);
+  if (newIndex > existing.length - 1 || newIndex < 0) {
+    console.log("move edge case");
     return existing;
   }
   /*var indexes = [index, newIndex].sort(); //Sort the indixes
   array.splice(indexes[0], 2, array[indexes[1]], array[indexes[0]]); //Replace from lowest index, two elements, reverting the order*/
-  const mapping = {[index]: newIndex, [newIndex]: index};
+  const mapping = { [index]: newIndex, [newIndex]: index };
   for (let i = 0; i < existing.length; i++) {
-    array.push(existing[typeof mapping[i] === 'number' ? mapping[i] : i]);
+    array.push(existing[typeof mapping[i] === "number" ? mapping[i] : i]);
   }
   return array;
 };
 
-export const moveUp = function(array, element) {
+export const moveUp = function (array, element) {
   return move(array, element, -1);
 };
 
-export const moveDown = function(array, element) {
+export const moveDown = function (array, element) {
   return move(array, element, 1);
 };
 
 export const range = (start, end, step = 1) => {
-  const forLoop = fn => {
-    for (let x = start; x <= end; x += step) fn(x)
-  }
+  const forLoop = (fn) => {
+    for (let x = start; x <= end; x += step) fn(x);
+  };
 
-  const between = (v, start, end) => v >= start && v <= end
-  const hasValue = v => between(v, start, end) || between(v, end, start)
+  const between = (v, start, end) => v >= start && v <= end;
+  const hasValue = (v) => between(v, start, end) || between(v, end, start);
 
   const iterate = function* (mapFn) {
-    for (let x = start; x <= end; x += step) yield mapFn ? mapFn(x) : x
-  }
+    for (let x = start; x <= end; x += step) yield mapFn ? mapFn(x) : x;
+  };
 
-  const rangeObj = {}
+  const rangeObj = {};
 
-  const createProp = v => ({ value: v })
-  const map = createProp(mapFn => [...iterate(mapFn)])
-  const forEach = createProp(forLoop)
-  const includes = createProp(v => {
+  const createProp = (v) => ({ value: v });
+  const map = createProp((mapFn) => [...iterate(mapFn)]);
+  const forEach = createProp(forLoop);
+  const includes = createProp((v) => {
     for (let x = start; x <= end; x += step) {
-      if (v === x) return true
+      if (v === x) return true;
     }
-    return false
-  })
-  const has = createProp(hasValue)
+    return false;
+  });
+  const has = createProp(hasValue);
 
   Object.defineProperties(rangeObj, {
     map,
     forEach,
     includes,
     has,
-  })
-  return rangeObj
-}
+  });
+  return rangeObj;
+};
 
 export const capitalize = (s) => {
   if (!s) return s;
@@ -90,26 +90,28 @@ export function orderBy(arr, selector, desc = false) {
     if (a == b) return 0;
     return (desc ? a > b : a < b) ? -1 : 1;
   });
-};
+}
 
-export function jj(data) { return JSON.parse(JSON.stringify(data)); };
+export function jj(data) {
+  return JSON.parse(JSON.stringify(data));
+}
 
+export function removeOptions(options, notAllowedOptions) {
+  return options
+    ?.filter(({ value }) => {
+      return !value || !notAllowedOptions.has(value);
+    })
+    .map(({ value, options, ...props }) => {
+      if (options) {
+        return { ...props, options: removeOptions(options, notAllowedOptions) };
+      } else {
+        return { value, ...props };
+      }
+    });
+}
 
-export function removeOptions (options, notAllowedOptions) {
-  return options?.filter(({ value }) => {
-    return !value || !notAllowedOptions.has(value);
-  }).map(({ value, options, ...props }) => {
-    if (options) {
-      return { ...props, options: removeOptions(options, notAllowedOptions) };
-    } else {
-      return { value, ...props };
-    }
-  });
-};
-
-
-export function updateFieldDefinition (fields, k, updateFn) {
-  return fields?.map(field => {
+export function updateFieldDefinition(fields, k, updateFn) {
+  return fields?.map((field) => {
     if (field.k) {
       if (field.k === k) {
         return updateFn(field);
@@ -117,34 +119,46 @@ export function updateFieldDefinition (fields, k, updateFn) {
         return field;
       }
     } else if (field.fields) {
-      return {...field, fields: updateFieldDefinition(field.fields, k, updateFn)};
+      return {
+        ...field,
+        fields: updateFieldDefinition(field.fields, k, updateFn),
+      };
     } else {
       return field;
     }
   });
-};
+}
 
-
-export function modifyHelper (path, struct, f, variables) {
+export function modifyHelper(path, struct, f, variables) {
   if (!variables) variables = {};
   if (!path.length) return f(struct, variables);
   const [p, ...rest] = path;
   if (p === "*") {
-    return (struct || []).map(el => modifyHelper(rest, el, f, variables));
+    return (struct || []).map((el) => modifyHelper(rest, el, f, variables));
   } else if (typeof p === "string") {
-    return {...struct, [p]: modifyHelper(rest, struct[p], f, variables)};
+    return { ...struct, [p]: modifyHelper(rest, struct[p], f, variables) };
   } else if (p.oneOf) {
-    const result = {...struct};
+    const result = { ...struct };
     for (const k of p.oneOf) {
       //console.log('visiting oneOf', k, struct[k]);
-      result[k] = modifyHelper(rest, struct[k], f, p.k ? {...variables, [p.k]: k} : variables);
+      result[k] = modifyHelper(
+        rest,
+        struct[k],
+        f,
+        p.k ? { ...variables, [p.k]: k } : variables
+      );
     }
     return result;
   } else if (p.matching) {
-    return (struct || []).map(el => {
+    return (struct || []).map((el) => {
       const matching = p.matching(el, variables);
       if (matching) {
-        return modifyHelper(rest, el, f, p.k ? {...variables, [p.k]: matching} : variables);
+        return modifyHelper(
+          rest,
+          el,
+          f,
+          p.k ? { ...variables, [p.k]: matching } : variables
+        );
       } else {
         return el;
       }
@@ -152,16 +166,16 @@ export function modifyHelper (path, struct, f, variables) {
   }
 }
 
-export function zipArrays(a,b){
-    // pre-allocate an array to hold the results
-    const rval=Array(Math.max(a.length, b.length));
-    for(let i=0; i<rval.length; i++){
-        rval[i]=[a[i],b[i]]
-    }
-    return rval
+export function zipArrays(a, b) {
+  // pre-allocate an array to hold the results
+  const rval = Array(Math.max(a.length, b.length));
+  for (let i = 0; i < rval.length; i++) {
+    rval[i] = [a[i], b[i]];
+  }
+  return rval;
 }
 
-export function pathToUpdate (path, value) {
+export function pathToUpdate(path, value) {
   let v = {},
     p,
     ee,
@@ -177,7 +191,7 @@ export function pathToUpdate (path, value) {
   }
   p[vv_key][ee] = value;
   return r;
-};
+}
 
 export function getByPath(struct, path) {
   let v = struct;
@@ -189,7 +203,7 @@ export function getByPath(struct, path) {
 }
 
 export function setByPath(struct, path, value) {
-  return update(value, pathToUpdate(path, {$set: value}));
+  return update(value, pathToUpdate(path, { $set: value }));
 }
 
 export { update };
--- frontend/src/runModal.js
+++ frontend/src/runModal.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect, useRef, memo, createContext } from "react";
 import {
-  formComponents,
-  validateDefinition,
-  definitionIsInvalid,
-  FormComponent,
-  GenericForm,
-  formValidators,
-  fieldsLayouts,
-  FieldLabel,
+	formComponents,
+	validateDefinition,
+	definitionIsInvalid,
+	FormComponent,
+	GenericForm,
+	formValidators,
+	fieldsLayouts,
+	FieldLabel,
 } from "./logicore-forms";
 import { v4 as uuidv4 } from "uuid";
 import classd from "classd";
 import _ from "lodash";
 import {
-  pathToUpdate,
-  getByPath,
-  setByPath,
-  update,
+	pathToUpdate,
+	getByPath,
+	setByPath,
+	update,
 } from "./logicore-forms/utils";
 
 import { Button, Modal } from "react-bootstrap";
@@ -24,30 +24,31 @@ import { Button, Modal } from "react-bootstrap";
 const ModalContext = createContext(null);
 
 const ModalProvider = ({ children }) => {
-  const [value, onChange] = useState(null);
-  const [show, setShow] = useState(false);
-  const [errors, setErrors] = useState(null);
-  const [config, setConfig] = useState({});
-  const [cb, setCallback] = useState({});
-  const context = config?.context || {};
-  /*useEffect(() => {
+	const [value, onChange] = useState(null);
+	const [show, setShow] = useState(false);
+	const [errors, setErrors] = useState(null);
+	const [config, setConfig] = useState({});
+	const cb = useRef(null);
+	const context = config?.context || {};
+	/*useEffect(() => {
     setState(value);
     setErrors(null);
   }, [show]);*/
-  const onReset1 = (path) => {
-    setErrors(update(errors, pathToUpdate(path, { $set: null })), null);
-  };
-  const handleClose = (_) => setShow(false);
-  const handleSubmit = () => {
-    const error = validateDefinition(config?.fields, value);
-    setErrors(error);
-    if (!definitionIsInvalid(config?.fields, error, value)) {
-      // ok
-      cb && cb(value);
-      //onReset(path);
-      handleClose();
-    } else {
-      /*NotificationManager.error(
+	const onReset1 = (path) => {
+		setErrors(update(errors, pathToUpdate(path, { $set: null })), null);
+	};
+	const handleClose = (_) => setShow(false);
+	const handleSubmit = () => {
+		const error = validateDefinition(config?.fields, value);
+		setErrors(error);
+		if (!definitionIsInvalid(config?.fields, error, value)) {
+			// ok
+			console.log("run cb");
+			cb?.current(value);
+			//onReset(path);
+			handleClose();
+		} else {
+			/*NotificationManager.error(
         "Please fix the errors below",
         "Error"
       );
@@ -60,56 +61,56 @@ const ModalProvider = ({ children }) => {
           console.warn(e);
         }
       }, 50);*/
-    }
-  };
-  const runModal = (config, value, cb) => {
-    setConfig(config);
-    onChange(value);
-    setCallback(cb);
-    setShow(true);
-  };
-  //container={(_) => document.getElementById("bootstrap-modals")}
-  return (
-    <ModalContext.Provider value={{ runModal }}>
-      <Modal
-        show={show}
-        onHide={handleClose}
-        animation={false}
-        size={config?.modalSize || "lg"}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>{config.title || "Edit"}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div>
-            <FormComponent
-              definition={{ ...config?.fields, layout: void 0 }}
-              value={value}
-              onChange={onChange}
-              error={errors}
-              onReset={onReset1}
-              path={[]}
-              context={{
-                ...context,
-                forceLabelWidth: "100%",
-                labelPlacement: "horizontalPlus",
-                handleSubmit,
-              }}
-            />
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleSubmit}>
-            OK
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      {children}
-    </ModalContext.Provider>
-  );
+		}
+	};
+	const runModal = (config, value, cbNew) => {
+		setConfig(config);
+		onChange(value);
+		cb.current = cbNew;
+		setShow(true);
+	};
+	//container={(_) => document.getElementById("bootstrap-modals")}
+	return (
+		<ModalContext.Provider value={{ runModal }}>
+			<Modal
+				show={show}
+				onHide={handleClose}
+				animation={false}
+				size={config?.modalSize || "lg"}
+			>
+				<Modal.Header closeButton>
+					<Modal.Title>{config.title || "Edit"}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<div>
+						<FormComponent
+							definition={{ ...config?.fields, layout: void 0 }}
+							value={value}
+							onChange={onChange}
+							error={errors}
+							onReset={onReset1}
+							path={[]}
+							context={{
+								...context,
+								forceLabelWidth: "100%",
+								labelPlacement: "horizontalPlus",
+								handleSubmit,
+							}}
+						/>
+					</div>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={handleSubmit}>
+						OK
+					</Button>
+				</Modal.Footer>
+			</Modal>
+			{children}
+		</ModalContext.Provider>
+	);
 };
 
 export { ModalProvider, ModalContext };
