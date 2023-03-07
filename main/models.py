dca35f53f2ccd7a8939a4308167bd1040af09097
Andrew Boltachev <andrewboltachev@gmail.com>
2023-03-07 03:52:07 +0300

    getNone

--- main/models.py
+++ main/models.py
@@ -38,9 +38,8 @@ class Stratagem(models.Model):
     def __str__(self):
         return self.name
 
-
     class Meta:
-        ordering = ['-modified_dt']
+        ordering = ["-modified_dt"]
 
 
 class CodeFormat(StatusOptions):
@@ -66,9 +65,8 @@ class CodeSearch(models.Model):
     def __str__(self):
         return self.name
 
-
     class Meta:
-        ordering = ['-modified_dt']
+        ordering = ["-modified_dt"]
 
 
 class MatcherFiddle(models.Model):
@@ -79,48 +77,45 @@ class MatcherFiddle(models.Model):
     created_dt = models.DateTimeField(auto_now_add=True)
     modified_dt = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
     )
     session_id = models.CharField(max_length=128, null=True, blank=True, default=None)
 
     def __str__(self):
         return self.name
 
-
     class Meta:
-        ordering = ['-modified_dt']
+        ordering = ["-modified_dt"]
+
 
 # Fiddle
+
+
+def getNone():
+    return None
 
 
 class Fiddle(models.Model):
     # Definition
     kind = FiddleType.as_choices()
-    version = models.CharField(default='000', max_length=3)
+    version = models.CharField(default="000", max_length=3)
     # Revision
     uuid = models.UUIDField(default=uuid.uuid4)
     rev = models.PositiveBigIntegerField(default=1)
     parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.SET_NULL)
     # Data
     name = models.CharField(max_length=1024, blank=True, default="")
-    data = models.JSONField(default=lambda: None, blank=False, null=False)
+    data = models.JSONField(default=getNone, blank=False, null=False)
     # Timestamp
     created_dt = models.DateTimeField(auto_now_add=True)
     modified_dt = models.DateTimeField(auto_now=True)
     # Ownership
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
     )
 
     def __str__(self):
         return self.name
 
-
     class Meta:
-        ordering = ['-modified_dt']
+        ordering = ["-modified_dt"]
