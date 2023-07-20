11db03a1069232b5f054f49fe14b22c7ad170051
Andrew Boltachev <andrewboltachev@gmail.com>
2023-07-20 09:25:51 +0300

    add fields

--- main/admin.py
+++ main/admin.py
@@ -3,4 +3,10 @@ from . import models
 
 
 admin.site.register(models.CodeSearch)
-admin.site.register(models.Log)
+
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ["pk", "created_dt"]
+
+
+admin.site.register(models.Log, LogAdmin)
