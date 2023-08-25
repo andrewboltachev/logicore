60e03237d300369c115a0dc675d6401addd9dca1
Andrew Boltachev <andrewboltachev@gmail.com>
2023-08-25 22:31:45 +0300

    Add author and book

--- main/admin.py
+++ main/admin.py
@@ -10,3 +10,7 @@ class LogAdmin(admin.ModelAdmin):
 
 
 admin.site.register(models.Log, LogAdmin)
+
+
+admin.site.register(models.Author)
+admin.site.register(models.Book)
--- main/models.py
+++ main/models.py
@@ -125,3 +125,19 @@ class Log(models.Model):
     created_dt = models.DateTimeField()
     data = models.JSONField(default=None, null=True, blank=True)
     data2 = models.JSONField(default=None, null=True, blank=True)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.author} - {self.name}"
