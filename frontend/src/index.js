52d0688b741dc7eccf9118ccbce2604dcc5efe00
Andrei Boltachev <andrewboltachev@gmail.com>
2024-01-26 08:53:23 +0300

    stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//import * as nodes from "./nodes";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
