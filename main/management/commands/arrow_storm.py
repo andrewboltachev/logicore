eed074a2141b030ab8ba2a679cbb051d25a1080e
Andrei Boltachev <andrewboltachev@gmail.com>
2024-03-16 14:20:30 +0300

    arrow_storm

---
+++ main/management/commands/arrow_storm.py
@@ -0,0 +1,25 @@
+import json
+from django.core.management.base import BaseCommand, CommandError
+from main import models
+
+
+def handle(e):
+    if not e["sourceHandle"]:
+        e["sourceHandle"] = "arrowSource"
+    if not e["targetHandle"]:
+        e["targetHandle"] = "arrowTarget"
+
+
+class Command(BaseCommand):
+    help = "Hello world"
+
+    def handle(self, *args, **options):
+        for f in models.Fiddle.objects.filter(kind="LOGICORE1"):
+            if "nodes" not in f.data:
+                continue
+            if "edges" not in f.data:
+                continue
+            f.data["edges"] = [
+                handle(e) for e in f.data["edges"] 
+            ]
+            f.save()
