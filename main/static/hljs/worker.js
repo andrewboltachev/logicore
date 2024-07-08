f09c34d3138ef48d61721ad3cfcc476199a1e872
Andrei Boltachev <andrewboltachev@gmail.com>
2024-07-08 16:12:58 +0300

    cleanup

--- main/static/hljs/worker.js
+++ main/static/hljs/worker.js
@@ -3,8 +3,6 @@ onmessage = function (evt) {
     '/static/hljs/highlight.min.js',
     '/static/hljs/typescript.min.js'
   );
-  //self.hljs.registerLanguage('typescript', ts);
   const result = self.hljs.highlight('typescript', evt.data);
   postMessage(result.value);
-  //postMessage(self.hljs.listLanguages());
 }
