668ecf916b0dee524e13fed4663706b2a29a733e
Andrei Boltachev <andrewboltachev@gmail.com>
2025-01-04 15:25:00 +0300

    useLocalStorage

--- frontend/src/python_01_explorer.jsx
+++ frontend/src/python_01_explorer.jsx
@@ -3,9 +3,15 @@ import React, {useContext, useState} from "react";
 import {getByPath, setByPath} from "./logicore-forms";
 import {ModalContext} from "./runModal";
 import _ from "lodash";
+import { useLocalStorage } from "@uidotdev/usehooks";
+
+import hljs from 'highlight.js/lib/core';
+import python from 'highlight.js/lib/languages/python';
+hljs.registerLanguage('python', python);
+
 
 const Python01Explorer = () => {
-    const [code, setCode] = useState("");
+    const [code, setCode] = useLocalStorage("PYTHON_01_EXPLORER_CODE", "");
     const [tree, setTree] = useState(null);
     const { runModal } = useContext(ModalContext);
     let t = _.identity;
@@ -25,13 +31,14 @@ const Python01Explorer = () => {
                 <h5>
                   Python code
                     <button className="btn btn-sm btn-primary mx-2" onClick={(e) => {
-                        /*const result = await runModal({
+                      (async () => {
+                        const result = await runModal({
                             title: t("Insert Python code"),
                             fields: {
                                 type: "Fields",
                                 fields: [
                                     {
-                                        type: "TextField",
+                                        type: "TextareaField",
                                         k: "val",
                                         label: t("Code"),
                                         required: false,
@@ -44,21 +51,17 @@ const Python01Explorer = () => {
                             },
                         });
                         if (result) {
-                            /*const current = { ...getByPath(value, path) };
-                            delete current[k];
-                            current[result.val] = v;
-                            onChange(setByPath(value, path, current));* /
-                        }*/
+                            setCode(result.val);
+                        }
                         console.log('ahah');
+                      })();
                     }}
                     >
                        Insert Python Code 
                     </button>
                 </h5>
                 </div>
-                <textarea
-                    className="form-control flex-grow-1"
-                />
+                <textarea className="form-control flex-grow-1" readOnly value={code} />
             </div>
         </div>
         </div>
