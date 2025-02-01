39e025bbb5532e3291babdb84a49ce6d617ee5c7
Andrei Boltachev <andrewboltachev@gmail.com>
2025-02-01 20:07:59 +0300

    use second layer

--- frontend/src/python_01_explorer.jsx
+++ frontend/src/python_01_explorer.jsx
@@ -21,86 +21,77 @@ function getRectParams(r) {
 }
 
 const Python01Explorer = () => {
-  // Основной
+    // Основной
     const [code, setCode] = useLocalStorage("PYTHON_01_EXPLORER_CODE", "");
-  // Производные
+    // Производные
     const [tree, setTree] = useState(null);
     const [highlighted, setHighlighted] = useState("");
 
-  useEffect(() => {
-    const highlighted = hljs.highlight("python", code);
-    setHighlighted(highlighted.value);
-  }, [code]);
+      useEffect(() => {
+        const highlighted = hljs.highlight("python", code);
+        setHighlighted(highlighted.value);
+      }, [code]);
 
-    const { runModal } = useContext(ModalContext);
-    let t = _.identity;
-    const [selection, setSelection] = useState(null);
-    const onSelectionChange = useCallback((e) => {
-      setSelection(_ => window.getSelection());
-    })
-    const codeRef = useRef(null);
-    useEffect(() => {
-      console.log('curr', codeRef.current);
-        document.addEventListener('selectionchange', onSelectionChange);
-        return () => {
-          document.removeEventListener('selectionchange', onSelectionChange);
-        };
-    }, []);
-    return (
-        <div className="container-fluid flex-grow-1 d-flex py-3" style={{overflow: "hidden"}}>
-        <div className="row align-items-stretch flex-grow-1" style={{overflow: "hidden"}}>
-            <div className="col d-flex flex-column">
-                <h5>
-                    Grammar (Pseudo-Python)!{" "}
-                </h5>
-                <textarea
-                    className="form-control flex-grow-1"
-                />
-            </div>
-            <div className="col d-flex flex-column" style={{overflow: "hidden"}}>
-                <div style={{overflow: "hidden"}}>
-                <h5>
-                  Python code
-                    <button className="btn btn-sm btn-primary mx-2" onClick={(e) => {
-                      (async () => {
-                        const result = await runModal({
-                            title: t("Insert Python code"),
-                            fields: {
-                                type: "Fields",
-                                fields: [
-                                    {
-                                        type: "TextareaField",
-                                        k: "val",
-                                        label: t("Code"),
-                                        required: false,
-                                    },
-                                ],
-                            },
-                            modalSize: "md",
-                            value: {
-                                val: '',
-                            },
-                        });
-                        if (result) {
-                            setCode(result.val);
-                        }
-                        console.log('ahah');
-                      })();
-                    }}
-                    >
-                       Insert Python Code 
-                    </button>
-                </h5>
+        const { runModal } = useContext(ModalContext);
+        let t = _.identity;
+        const codeRef = useRef(null);
+        return (
+            <div className="container-fluid flex-grow-1 d-flex py-3" style={{overflow: "hidden"}}>
+            <div className="row align-items-stretch flex-grow-1" style={{overflow: "hidden"}}>
+                <div className="col d-flex flex-column">
+                    <h5>
+                        Grammar (Pseudo-Python)!{" "}
+                    </h5>
+                    <textarea
+                        className="form-control flex-grow-1"
+                    />
                 </div>
-                <div className="form-control flex-grow-1" style={{flex: 1, position: "relative", overflow: "auto"}}>
-                  <div style={{position: "absolute", top: 0, left: 0, right: 0, bottom: 0, margin: "6px 12px" }}>
-                    {selection && <div style={{backgroundColor: 'yellow', position: 'fixed', ...getRectParams(selection.getRangeAt(0).getBoundingClientRect())}} />}
-                    <div ref={codeRef} id="python_01_explorer_code" dangerouslySetInnerHTML={{__html: highlighted}}></div>
-                  </div>
+                <div className="col d-flex flex-column" style={{overflow: "hidden"}}>
+                    <div style={{overflow: "hidden"}}>
+                    <h5>
+                      Python code
+                        <button className="btn btn-sm btn-primary mx-2" onClick={(e) => {
+                          (async () => {
+                            const result = await runModal({
+                                title: t("Insert Python code"),
+                                fields: {
+                                    type: "Fields",
+                                    fields: [
+                                        {
+                                            type: "TextareaField",
+                                            k: "val",
+                                            label: t("Code"),
+                                            required: false,
+                                        },
+                                    ],
+                                },
+                                modalSize: "md",
+                                value: {
+                                    val: '',
+                                },
+                            });
+                            if (result) {
+                                setCode(result.val);
+                            }
+                            console.log('ahah');
+                          })();
+                        }}
+                        >
+                           Insert Python Code
+                        </button>
+                    </h5>
+                    </div>
+                    <div className="form-control flex-grow-1" style={{flex: 1, position: "relative", overflow: "auto"}}>
+                      <div style={{position: "absolute", top: 0, left: 0, right: 0, bottom: 0, margin: "6px 12px" }}>
+                          <div style={{position: "absolute", whiteSpace: "pre"}}>{Array.from(code).map((ch, i) => (<span key={i} style={{
+                              color: "transparent"
+                          }}>{ch}</span>))}</div>
+                          {<div style={{pointerEvents: "none"}} ref={codeRef} id="python_01_explorer_code" dangerouslySetInnerHTML={{__html: highlighted}}></div>}
+                      </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        </div>
+            </div>
     );
 }
 
