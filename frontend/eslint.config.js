b52fc4c6a02b1f42aaf6d71849a4b75b6c0481bc
Andrei Boltachev <andrewboltachev@gmail.com>
2025-02-27 21:50:39 +0300

    flat plugin fix

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,9 @@
 import globals from 'globals'
+import { fixupPluginRules } from "@eslint/compat";
 import pluginJs from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -13,6 +16,18 @@ export default [
       react: {
         version: "detect",
       },
+    },
+  },
+  // Even though eslint-plugin-react-hooks exposes configs.recommended, it is not yet compatible with the flat file config,
+  // because it has plugins: [ 'react-hooks' ] property, but plugins should be an object
+  // Once it is supported, replace with: eslintPluginReactHooks.configs.recommended,
+  {
+    plugins: {
+      //"react": reactPlugin, // remove this if you already have another config object that adds the react plugin
+      "react-hooks": fixupPluginRules(pluginReactHooks),
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
     },
   },
 ]
