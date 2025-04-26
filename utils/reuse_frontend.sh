86d96268e82a4f91b41555c549856559b57c7e22
Andrei Boltachev <andrewboltachev@gmail.com>
2025-04-26 12:26:13 +0300

    fix script

--- utils/reuse_frontend.sh
+++ utils/reuse_frontend.sh
@@ -3,12 +3,12 @@
 echo 'Use the latest development frontend files for production'
 
 [ -d frontend/dist ] || exit 1
-rm -rf static/react/
+rm -rf main/static/react/
 mkdir -p static
-cp -r -v frontend/dist static/react
+cp -r -v frontend/dist main/static/react
 mkdir -p ./main/templates/react/
 
-find static/react/ -name "index-*.js" | xargs sed -e "s#/assets/#/static/react/assets/#g" -i
+find main/static/react/ -name "index-*.js" | xargs sed -e "s#/assets/#/static/react/assets/#g" -i
 cat frontend/dist/index.html | sed -e "s#/assets/#/static/react/assets/#g" | grep module > ./main/templates/react/bundle_js.html
 cat frontend/dist/index.html | sed -e "s#/assets/#/static/react/assets/#g" | grep stylesheet  > ./main/templates/react/bundle_css.html
 
