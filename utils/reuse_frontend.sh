8a1d8d7acc76b98cfa279e1e8c67fed92472cecf
Andrei Boltachev <andrewboltachev@gmail.com>
2025-04-26 10:59:36 +0300

    change file

--- utils/reuse_frontend.sh
+++ utils/reuse_frontend.sh
@@ -3,11 +3,13 @@
 echo 'Use the latest development frontend files for production'
 
 [ -d frontend/dist ] || exit 1
-rm -rf main/static/react/
-cp -r -v frontend/dist/ main/static/react/
-rm -rf ./main/templates/react/
+rm -rf static/react/
+mkdir -p static
+cp -r -v frontend/dist static/react
 mkdir -p ./main/templates/react/
-cat main/static/react/index.html | grep module | sed -e "s#assets#static/react/assets#" > ./main/templates/react/bundle_js.html
-cat main/static/react/index.html | grep stylesheet | sed -e "s#assets#static/react/assets#" > ./main/templates/react/bundle_css.html
+
+find static/react/ -name "index-*.js" | xargs sed -e "s#/assets/#/static/react/assets/#g" -i
+cat frontend/dist/index.html | sed -e "s#/assets/#/static/react/assets/#g" | grep module > ./main/templates/react/bundle_js.html
+cat frontend/dist/index.html | sed -e "s#/assets/#/static/react/assets/#g" | grep stylesheet  > ./main/templates/react/bundle_css.html
 
 echo 'Build files copied.'
