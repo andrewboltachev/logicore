0bd93e879f536245c233e94ff977a942b7c95c11
Andrei Boltachev <andrewboltachev@gmail.com>
2025-05-26 09:49:24 +0300

    some

--- main/management/commands/cr.py
+++ main/management/commands/cr.py
@@ -1,6 +1,5 @@
-import copy
+import re
 import sys
-from pathlib import Path
 
 import tqdm
 
@@ -62,6 +61,136 @@ def walk2(node, handler):
     return f(node)
 
 
+def get_spellings(term):
+    """
+    Convert term into all spellings
+    :param term: term, e.g. "foo-bar"
+    :return: list of spellings, e.g.:
+    ['foobar',
+     'foo bar',
+     'foo-bar',
+     'foo_bar',
+     'FOOBAR',
+     'FOO BAR',
+    ...]
+    """
+    options = []
+    lower = [p.lower() for p in term.replace("_", "-").split("-")]
+    upper = [p.upper() for p in lower]
+    capitalize = [p.capitalize() for p in lower]
+    capitalize_first = [capitalize[0]] + lower[1:]
+    ig = [upper[0]] + capitalize[1:]
+    capitalize_rest = [lower[0]] + capitalize[1:]
+    for cased in [
+        lower,
+        upper,
+        capitalize,
+        capitalize_first,
+        ig,  # FIXME
+        capitalize_rest,  # camelCase ?
+    ]:
+        for joint in ["", " ", "-", "_"]:
+            options.append(joint.join(cased))
+    return options
+
+
+def make_replace_fn(regexp, terms, replace):
+    replace_map = {}
+    if terms and len(terms[0].split("-")) > 1:
+        replace_spellings = get_spellings(replace)
+        for term in terms:
+            replace_map.update(
+                dict(map(lambda a, b: [a, b], get_spellings(term), replace_spellings))
+            )
+    else:
+        t = terms[0]
+        r = replace.split("-")
+        replace_map = {
+            t.lower(): "_".join([w.lower() for w in r]),
+            t.lower().capitalize(): "".join([w.lower().capitalize() for w in r]),
+            t.upper(): "_".join([w.upper() for w in r]),
+        }
+    print("Replace map")
+    print(replace_map)
+
+    def replace_fn(value, *args, **kwargs):
+        if type(value) != str:
+            return value
+
+        def repl(matchobj):
+            value = matchobj.group()
+            if matched := replace_map.get(value, None):
+                return matched
+            else:
+                print(f"WARNING: no match for {value}")
+                return value
+
+        return re.sub(regexp, repl, value)
+
+    return replace_fn
+
+
+def f1(term, replacement):
+    variants = []
+    terms = [term]
+    for v in terms:
+        parts = [re.escape(p) for p in v.split("-")]
+        variants.append(r"[ \-\_]?".join(parts))
+    regexp = f"({'|'.join(variants)})"
+    result_parts = [re.escape(p) for p in replacement.split("-")]
+    result_regexp = r"[ \-\_]?".join(result_parts)
+    regexp = re.compile(regexp, flags=re.I)
+    # fns
+    # {'avito': 'telegram_personal', 'Avito': 'TelegramPersonal', 'AVITO': 'TELEGRAM_PERSONAL'}
+    # Out[3]: <function main.management.commands.cr.make_replace_fn.<locals>.replace_fn(value, *args, **kwargs)>
+
+    print("The regexp", regexp)
+    return make_replace_fn(regexp, terms, replacement)
+
+
+def byone_replacer(name_from, name_to):
+    def f(term):
+        nonlocal name_from, name_to
+        # "lower"
+        variant_from = name_from.lower()
+
+        if variant_from in term:
+            chunks = name_to.lower().split("-")
+            if "-" in term:
+                variant_to = "-".join(chunks)
+            elif "_" in term:
+                variant_to = "_".join(chunks)
+            else:
+                print(f"WARNING: falling back to underscore-joined for {term}")
+                variant_to = "_".join(chunks)
+            return term.replace(variant_from, variant_to)
+
+        # "upper"
+        variant_from = name_from.upper()
+        if variant_from in term:
+            chunks = name_to.upper().split("-")
+            if "-" in term:
+                variant_to = "-".join(chunks)
+            elif "_" in term:
+                variant_to = "_".join(chunks)
+            else:
+                print(f"WARNING: falling back to underscore-joined for {term}")
+                variant_to = "_".join(chunks)
+            return term.replace(variant_from, variant_to)
+
+        # "Capitalized"
+        variant_from = name_from.lower().capitalize()
+        if variant_from in term:
+            chunks = [chunk.capitalize() for chunk in name_to.lower().split("-")]
+            variant_to = "".join(chunks)
+            return term.replace(variant_from, variant_to)
+
+        print(f"WARNING: didn't find value in: {term}")
+        return term
+
+    return f
+
+
 class Command(BaseCommand):
     help = 'Creates a RootedCopy model instance with provided fs_path, name_from, and name_to.'
 
@@ -78,6 +207,11 @@ class Command(BaseCommand):
             sys.stderr.write(f'RootedCopy {options["id"]} does not exist.\n')
             sys.exit(1)
 
+        if "-" not in rc.name_from and "-" in options["name_to"]:
+            replacer = byone_replacer(rc.name_from, options['name_to'])
+        else:
+            replacer = f1(rc.name_from, options['name_to'])
+
         for python_file in tqdm.tqdm(rc.files.split("\n")):
             filename = python_file
             code = read_file(filename)
@@ -88,12 +222,9 @@ class Command(BaseCommand):
             current_cancelled_child_paths = rc.cancelled_items.get(python_file, [])
 
             def handler(parent_path, node):
-                print(parent_path)
-                print(current_parent_paths)
-                print()
                 if parent_path in current_parent_paths:
                     the_map = {
-                        parent_path.replace(f"{parent_path}.", ""): item
+                        current_child_path.replace(f"{parent_path}.", "") + ".value": item  # TODO
                         for current_child_path, item in current_child_paths.items()
                         if (
                             current_child_path.startswith(f"{parent_path}.")
@@ -101,8 +232,9 @@ class Command(BaseCommand):
                         )
                     }
                     def handler2(node, child_path):
+                        # print(child_path, the_map.keys())
                         if v := the_map.get(child_path):
-                            return node.replace(v['spelling'], options['name_to'])
+                            return replacer(node)
                         return node
                     return walk2(node, handler2)
                 return None
