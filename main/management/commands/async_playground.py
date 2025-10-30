40446060095edfa4b1b8b5223f97f5cfb3969179
Andrei Boltachev <andrewboltachev@gmail.com>
2025-10-30 22:35:44 +0300

    some

--- main/management/commands/async_playground.py
+++ main/management/commands/async_playground.py
@@ -1,11 +1,28 @@
 import asyncio
 
+asyncio.sleep()
 
-async def foo():
-    await asyncio.sleep(1)
-    pass
+# давай первое значение
+# давай следующее значение, и прими x
+# давай следующее значение
+# ...
 
+def gen():
+    print("before")
+    try:
+        x = yield 42
+    except GeneratorExit:
+        pass
+    else:
+        print("x", x)
+    yield 2
+    yield 3
+    print("after")
+    return 111
 
-@asyncio.coroutine
-def aha():
-    pass
+if __name__ == '__main__':
+    g = gen()
+    g.send(None)
+    g.close()
+    g.send("foo!")
+    next(g)
